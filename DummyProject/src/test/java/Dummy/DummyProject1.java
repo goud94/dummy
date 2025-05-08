a933c2af40220bf8d085605a675b4a139769c142
Prasanth Dora <Prasanth Dora@Prashanth>
2025-05-08 12:43:50 +0530

    updated case1

--- DummyProject/src/test/java/Dummy/DummyProject1.java
+++ DummyProject/src/test/java/Dummy/DummyProject1.java
@@ -1,11 +1,17 @@
 package Dummy;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class DummyProject1 {
 	@Test
 	public void addDummy(){
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		driver.manage().window().maximize();
 		Reporter.log("Dummy is executed",true);
 	}
 
