763cbf26bb19f22e413aa744f83fc79a2b5bcefb
Prasanth Dora <Prasanth Dora@Prashanth>
2025-05-08 09:47:53 +0530

    updated!!

---
+++ DummyProject/src/test/java/Dummy/DummyProject1.java
@@ -0,0 +1,12 @@
+package Dummy;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DummyProject1 {
+	@Test
+	public void addDummy(){
+		Reporter.log("Dummy is executed",true);
+	}
+
+}
